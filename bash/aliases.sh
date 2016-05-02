681319713ca4061565f2cb06e84e69b0fee61255
malramsay64 <malramsay64@gmail.com>
2016-05-02 19:09:43 +1000

    Adding a simple bashrc file
    Breaking the file up into a number of smaller components

---
+++ bash/aliases.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# bash aliases
+
+alias cp='cp -iv'       # Confirm overwrite of file, list files on copying
+alias mv='mv -iv'       # Confirm overwrite of file, list files on moving
+alias la='ls -a'        # List hidden files
+alias ll='ls -l'        # List in long form
+alias mkdir='mkdir -pv' # Make all folders in path given, lists directories as they are created
+alias ..='cd ..'
+cd() { builtin cd "$@"; [ $? == 0 ] && ls -G ;}
+alias c='clear'
+del() { mv "$@" ~/.Trash/ ;}  # Make use of the trash on command line
+alias eject="hdiutil eject"   # will unmount and eject drives. useage eject /Volume/<drive>
+ip() { ipconfig getifaddr "$@" ;} # Get IP address
+alias rd='pushd +1'
+alias ravp='rsync -av --progress'
+alias grep='grep --color=AUTO'
+
+# Git aliases
+alias go='git checkout'
+alias gc='git commit'
+alias gb='git branch'
+alias gd='git diff'
+alias ga='git add'
+alias gs='git status'
+
