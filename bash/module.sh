c3cf994608783c9809b6db5bfa0c0eb46a2a7289
malramsay64 <malramsay64@gmail.com>
2016-06-27 10:35:08 +1000

    Changing bash equality operator

--- bash/module.sh
+++ bash/module.sh
@@ -6,6 +6,6 @@ MODULES="cmake cuda googletest benchmark autossh mpi/mpich-x86_64 lammps gnuplot
 
 if  hash module 2>/dev/null ; then
     for M in $MODULES; do
-        [[ $(module avail "$M" 2>&1 | wc -c) > 0 ]] && module load "$M" >/dev/null
+        [[ $(module avail "$M" 2>&1 | wc -c) -gt 0 ]] && module load "$M" >/dev/null
     done
 fi
