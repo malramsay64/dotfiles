49bdfac823c514d714a0e1c0ad5e3ef5da39f5d7
malramsay64 <malramsay64@gmail.com>
2016-09-10 14:01:19 +1000

    Updating install and mux files

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing dotfiles"
 
-#echo "Initialising submodule(s)"
+echo "Initialising submodule(s)"
 git submodule update --init --recursive
 
 source "install/link.sh"
@@ -12,6 +12,6 @@ mkdir -p "$HOME/.vim-tmp"
 [ -d vim/vim.symlink/bundle/Vundle.vim ] || git clone https://github.com/VundleVim/Vundle.vim.git vim/vim.symlink/bundle/Vundle.vim
 
 echo "Installing vim plugins"
-vim +PluginInstall +qall
+vim +PlugInstall +qall
 
 echo "Done!"
