d6d27a158d543cb9f4a9b7db0ac8792e55e254eb
Malcolm Ramsay <malramsay64@gmail.com>
2017-06-10 09:50:40 +1000

    Update zsh configuration
    Updating the configuration to remove code that is now unnecessary,
    having more commonality between bash and fixing some compatibility
    issues.

--- bash/environment.bash
+++ bash/environment.bash
@@ -1,6 +1,15 @@
+# A series of environment settings that should be present in whichever shell is being used
+
+# Add italics to the current term
+{ infocmp -1 $TERM ; echo -e "\tsitm=\\E[3m,\n\tritm=\\E[23m,"; } > ~/dotfiles/resources/$TERM.terminfo
+tic ~/dotfiles/resources/$TERM.terminfo
 
 # For homebrew
 export PATH=/usr/local/bin:$PATH
+
+# Change settings for z
+export _Z_DATA=$HOME/.z/z
+source $HOME/dotfiles/bash/z/z.sh
 
 # Setting editor to nvim if present
 if [ $(hash nvim 2>/dev/null && echo 1) ]; then
@@ -12,9 +21,9 @@ else
 fi
 
 # Adding home directory bin to path
-[ -d "$HOME/bin" ] && export PATH="$HOME/bin:$PATH"
+[ -d "$HOME/dotfiles/bin" ] && export PATH="$PATH:$HOME/dotfiles/bin"
+[ -d "$HOME/.bin" ] && export PATH="$HOME/.bin:$PATH"
 [ -d "$HOME/.local/bin" ] && export PATH="$HOME/.local/bin:$PATH"
-export PATH="$PATH:$HOME/dotfiles/bin"
 
 # Test interactive shell
 [[ $- == *i* ]] && stty -ixon
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,28 +2,7 @@
 
 autoload -U colors && colors
 
-function python_version() { 
-    echo "version: $(python -V 2>&1 | cut -f 2 -d ' ')" 
-}
-
-function _virtualenv_prompt_info {
-    if [[ -n "$(whence virtualenv_prompt_info)" ]]; then
-        if [ -n "$(whence pyenv_prompt_info)" ]; then
-            if [ "$1" = "inline" ]; then
-                ZSH_THEME_VIRTUAL_ENV_PROMPT_PREFIX="%{$fg[blue]%}python::%{$fg[red]%}"
-                ZSH_THEME_VIRTUAL_ENV_PROMPT_SUFFIX=""
-                virtualenv_prompt_info
-            fi
-            local _default_version="$(cat $PYENV_ROOT/version 2> /dev/null)"
-            [ "$(pyenv_prompt_info)" = "${_default_version}" ] && virtualenv_prompt_info
-        else
-            virtualenv_prompt_info
-        fi
-    fi
-}
-
-
-RPS1='$(git_super_status) $(_virtualenv_prompt_info) $(vi_mode_prompt_info)'
+RPS1='$(git_super_status) $(vi_mode_prompt_info)'
 PROMPT='%D{%H:%M} %{$fg[magenta]%}%m %{$fg[green]%}%3~ %{$reset_color%}%(!.#.$) '
 
 
