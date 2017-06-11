3961a1db5f8e72a96826d105d20fa216501beeae
Malcolm Ramsay <malramsay64@gmail.com>
2017-06-12 07:56:48 +1000

    Change zsh plugin manager to antibody

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,7 +2,7 @@
 
 autoload -U colors && colors
 
-RPS1='$(git_super_status) $(vi_mode_prompt_info)'
+RPS1='$(git_super_status)'
 PROMPT='%D{%H:%M} %{$fg[magenta]%}%m %{$fg[green]%}%3~ %{$reset_color%}%(!.#.$) '
 
 
