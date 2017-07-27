3835004012f0efac566f8f693d8fdecfcc2d773d
Malcolm Ramsay <malramsay64@gmail.com>
2017-07-27 13:23:15 +1000

    Automatically commit vimwiki wikis

---
+++ resources/wikicommit.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+#
+# wikicommit.sh
+# Copyright (C) 2017 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+(
+
+today = $(date +%Y-%m-%d)
+
+cd "$HOME/Documents/notes"
+git add PhD; git commit -m "PhD update $today"
+git add personal; git commit -m "Personal wiki update $today"
+git push
+
+cd "$HOME/Documents/notes/personal/diary"
+git add *; git commit -m "Diary update $today"
+git push
+
+)
+
