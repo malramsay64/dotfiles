f425135cdcb8fa6bf9f3a94bfa947ebba803fefb
Malcolm Ramsay <malramsay64@gmail.com>
2017-11-23 10:41:49 +1100

    Remove Vundle from installation

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ source "install/link.sh"
 
 echo "Creating vim directories"
 mkdir -p "$HOME/.vim-tmp/{swap,undo}"
-[ -d vim/vim.symlink/bundle/Vundle.vim ] || git clone https://github.com/VundleVim/Vundle.vim.git vim/vim.symlink/bundle/Vundle.vim
 
 echo "Installing vim plugins"
 vim +PlugUpdate +qall
