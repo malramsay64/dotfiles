aad9b8c2229a6fe578de43dd4dd57e4fc2ae3252
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-05 13:03:12 +1000

    Include colours for fish in repository
    This allows for a consistent interface across devices, having the same
    colourscheme. The installation just requires running the colours.fish
    file.

---
+++ colors.fish
@@ -0,0 +1,29 @@
+#!/usr/bin/env fish
+
+set fish_color_autosuggestion 969896
+set fish_color_cancel -r
+set fish_color_command b294bb
+set fish_color_comment f0c674
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end b294bb
+set fish_color_error cc6666
+set fish_color_escape bryellow --bold
+set fish_color_history_current --bold
+set fish_color_host normal
+set fish_color_match --background=brblue
+set fish_color_normal normal
+set fish_color_operator bryellow
+set fish_color_param 81a2be
+set fish_color_quote b5bd68
+set fish_color_redirection 8abeb7
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+set fish_pager_color_completion 
+set fish_pager_color_description B3A06D yellow
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_progress brwhite --background=cyan
+[31mF[31mi[31ms[31mh[31m [31mc[38;5;208mo[38;5;208ml[38;5;208mo[38;5;208mr[33ms[33m [33mh[33ma[32mv[32me[32m [32mb[34me[34me[34mn[34m [35ms[35me[35mt[30m(B[m
