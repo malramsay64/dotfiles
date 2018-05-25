60aef0b4e28e01f618409d120d5243156d7e80f5
Malcolm Ramsay <malramsay64@gmail.com>
2018-05-25 12:37:57 +1000

    Check directories exist before adding to path

--- config/fish/conf.d/000-env.fish
+++ config/fish/conf.d/000-env.fish
@@ -1,1 +1,12 @@
-set -x fish_user_paths $HOME/.pyenv/bin $HOME/.local/bin $HOME/.fzf/bin $HOME/dotfiles/bin $HOME/go/bin /usr/local/bin
+function __check_path_exists
+    if test -d $argv
+        echo $argv
+    end
+end
+
+set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/.pyenv/bin)
+set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/.local/bin)
+set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/.fzf/bin)
+set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/dotfiles/bin)
+set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/go/bin)
+set -x fish_user_paths $fish_user_paths (__check_path_exists /usr/local/bin)
