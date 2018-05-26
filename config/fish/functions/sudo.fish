4d2eea30c974a6ca1bccc8f347eb8792364fca01
Malcolm Ramsay <malramsay64@gmail.com>
2018-05-26 10:51:45 +1000

    Add override of sudo function in fish for !!
    I forget/don't realise I need to use sudo as a fairly regular
    occurrence. This enables the sudo !! functionality from bash/zsh running
    the previous command as sudo.

---
+++ config/fish/functions/sudo.fish
@@ -0,0 +1,10 @@
+# Provide bash/zsh like expansion of !! to the previous command
+
+function sudo
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end
+
