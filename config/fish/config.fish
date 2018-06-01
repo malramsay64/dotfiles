65bf9ba8a7405ac3c7437a73aa7921336c9f0db8
Malcolm Ramsay <malramsay64@gmail.com>
2018-06-01 11:47:39 +1000

    Modify PATH over fish_user_paths
    Updating the fish_user_paths variable was causing some unusual problems
    when dealing with pyenv shims. Often the order of PATH would be messed
    up since pyenv edited the PATH directly, and the fish_user_paths
    prepends the path resulting in the pyenv shims being halfway down the
    list of paths to search.
    
    This made it even more difficult to work out which version of things I
    was trying to use.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,12 +16,12 @@ function __check_path_exists
     end
 end
 
-set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/.pyenv/bin)
-set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/.local/bin)
-set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/.fzf/bin)
-set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/dotfiles/bin)
-set -x fish_user_paths $fish_user_paths (__check_path_exists $HOME/go/bin)
-set -x fish_user_paths $fish_user_paths (__check_path_exists /usr/local/bin)
+set -x PATH $PATH (__check_path_exists $HOME/.pyenv/bin)
+set -x PATH $PATH (__check_path_exists $HOME/.local/bin)
+set -x PATH $PATH (__check_path_exists $HOME/.fzf/bin)
+set -x PATH $PATH (__check_path_exists $HOME/dotfiles/bin)
+set -x PATH $PATH (__check_path_exists $HOME/go/bin)
+set -x PATH $PATH (__check_path_exists /usr/local/bin)
 
 if type -q nvim
     set -x EDITOR 'nvim'
