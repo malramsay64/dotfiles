0a51466a928ff80afad4d4eb31a19895d4fa146d
Malcolm Ramsay <malramsay64@gmail.com>
2018-07-17 15:53:32 +1000

    [zsh] Remove unused files

--- zsh/prompt.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/env zsh
-
-autoload -U colors && colors
-
-RPS1='$(pyenv version-name) $(git_super_status)'
-PROMPT='%D{%H:%M} %{$fg[magenta]%}%m %{$fg[green]%}%3~ %{$reset_color%}%(!.#.$) '
-
-
