10c4edb18e1e866c5b406ecde8a0c52a3dd00ec0
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-23 09:06:50 +1000

    fish: Install fisher if not installed
    This automatically installs fisher along with all the plugins listed in
    the fishfile when fisher is not already installed on the system. This
    makes it much easier to setup and configure fish appropriately on a new
    computer.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -43,6 +43,13 @@ direnv hook fish | source
 source $HOME/.config/fish/gnupg.fish
 source $HOME/.config/fish/alias.fish
 
+# Configure fisher if not installed
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 source (conda info --root)/etc/fish/conf.d/conda.fish
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
