9af1fdee69d5e163e8efd6c6acceb63413c9c360
Malcolm Ramsay <malramsay64@gmail.com>
2020-12-08 07:25:10 +1030

    chore: Create bash script to link directories
    This is a script to help automate the deployment and update of the
    config files.

---
+++ linking.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ensure all directories in config are linked to the ~/.config directory
+for dir in $(ls config/); do
+    # Use the -s flag to create a symbolic link
+    # Use the -i flag to intereactive determine overwriting of links
+    ln -si $(pwd)/$dir "$HOME"/.config/
+done
