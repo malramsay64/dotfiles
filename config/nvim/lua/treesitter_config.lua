c453a4c7481db5dfdc7dc8c5237e9d1521908e4e
Malcolm Ramsay <malramsay64@gmail.com>
2020-12-08 07:48:32 +1030

    nvim: Increase list of treesitter modules installed
    These are additional filetypes that are useful for working with

--- config/nvim/lua/treesitter_config.lua
+++ config/nvim/lua/treesitter_config.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
     -- one of "all", "language", or a list of languages
-    ensure_installed = {"rust", "python", "lua"},
+    ensure_installed = {"rust", "python", "lua", "toml", "yaml", "html", "typescript", "javascript", "cpp", "bash", "julia", "json"},
     highlight = {
         -- false will disable the whole extension
         enable = true,
