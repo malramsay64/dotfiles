321a7e9d9a6ba51556dae63ef9f7b25940e54d8f
Malcolm Ramsay <malramsay64@gmail.com>
2020-12-08 07:31:02 +1030

    nvim: Include lua utility for creating autocommands

--- config/nvim/lua/nvim_utils.lua
+++ config/nvim/lua/nvim_utils.lua
@@ -52,3 +52,15 @@ end
 function Command(name, code)
     vim.cmd("command! -nargs=0 " .. name .. " " .. code)
 end
+
+function nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        vim.api.nvim_command('augroup '..group_name)
+        vim.api.nvim_command('autocmd!')
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            vim.api.nvim_command(command)
+        end
+        vim.api.nvim_command('augroup END')
+    end
+end
