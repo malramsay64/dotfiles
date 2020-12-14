68c5bd29743ba60dd59fc936dc5d7e55593f80cd
Malcolm Ramsay <malramsay64@gmail.com>
2020-12-15 09:12:39 +1030

    zsh: Use more up to date conda zsh completion
    My version is unmaintained so this migrates to an alternative
    repository.

--- config/zsh/sourceables.sh
+++ config/zsh/sourceables.sh
@@ -12,8 +12,8 @@ source /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-
 fpath+=( /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
 fpath+=( /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-malramsay64-SLASH-conda-zsh-completion/conda.plugin.zsh
-fpath+=( /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-malramsay64-SLASH-conda-zsh-completion )
+source /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-esc-SLASH-conda-zsh-completion/conda-zsh-completion.plugin.zsh
+fpath+=( /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-esc-SLASH-conda-zsh-completion )
 source /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-srijanshetty-SLASH-zsh-pandoc-completion/zsh-pandoc-completion.plugin.zsh
 fpath+=( /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-srijanshetty-SLASH-zsh-pandoc-completion )
 source /home/malcolm/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ninrod-SLASH-pass-zsh-completion/pass-zsh-completion.plugin.zsh
