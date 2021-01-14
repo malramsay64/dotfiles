f94318d271555a96a38556bc6bb87f2efa91ab82
Malcolm Ramsay <malramsay64@gmail.com>
2021-01-15 09:24:34 +1030

    nvim: Update sumneko lua plugin
    This is no longer installed through the nvim-lspconfig package

--- config/nvim/lua/lsp_config.lua
+++ config/nvim/lua/lsp_config.lua
@@ -24,11 +24,26 @@ local custom_attach = function(client)
     BufMapper("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
 end
 
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
+
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
 nvim_lsp.sumneko_lua.setup({
     cmd = {
-        "~/.cache/nvim/nvim_lsp/sumneko_lua/lua-language-server/bin/Linux/lua-language-server",
+        sumneko_binary,
         "-E",
-        "~/.cache/nvim/nvim_lsp/sumneko_lua/lua-language-server/main.lua"
+        sumneko_root_path .. "/main.lua"
     },
     on_attach=custom_attach,
     settings = {
@@ -41,7 +56,7 @@ nvim_lsp.sumneko_lua.setup({
             workspace = {
                 library = {
                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("~/build/neovim/src/nvim/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
                 },
             },
         },
