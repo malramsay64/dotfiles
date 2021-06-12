a861f99db4de737ad845a62a89009c8b48a5ecc0
Malcolm Ramsay <malramsay64@gmail.com>
2021-06-12 20:00:11 +0930

    nvim: Add more treesitter languages to install

--- config/nvim/lua/treesitter_config.lua
+++ config/nvim/lua/treesitter_config.lua
@@ -1,13 +1,13 @@
 require('nvim-treesitter.configs').setup {
     -- one of "all", "language", or a list of languages
-    ensure_installed = {"rust", "python", "lua", "toml", "yaml", "html", "typescript", "javascript", "cpp", "bash", "julia", "json"},
+    ensure_installed = {"rust", "python", "lua", "toml", "yaml", "html", "typescript", "javascript", "cpp", "bash", "julia", "json", "css", "scss", "fish"},
     highlight = {
         -- false will disable the whole extension
         enable = true,
     },
     indent = {
         -- There are currently issues with the indentation
-        enable = false,
+        enable = true,
     },
     textobjects = {
         select = {
