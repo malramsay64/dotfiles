22d4daf57789e5c59724c6ab53cf95683ad5e04a
Malcolm Ramsay <m@malramsay.com>
2021-07-07 09:56:21 +0930

    nvim: Support pears nvim with compe

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -114,20 +114,21 @@ return require('packer').startup(function()
     -- Repeat plugin commands
     use 'tpope/vim-repeat'
 
-    -- use {
-    --     'steelsojka/pears.nvim',
-    --     config=function() require('pears').setup(function(conf)
-    --         conf.on_enter(function(pears_handle)
-    --             if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
-    --                 return vim.fn["compe#confirm"]("<CR>")
-    --             else
-    --                 pears_handle()
-    --             end
-    --         end)
-    --     end
-    --     )
-    --     end
-    -- }
+    use {
+        'steelsojka/pears.nvim',
+        config=function() require('pears').setup(function(conf)
+            conf.pair("<", {filetypes = {"typescript"}})
+            conf.on_enter(function(pears_handle)
+                if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
+                    return vim.fn["compe#confirm"]("<CR>")
+                else
+                    pears_handle()
+                end
+            end)
+        end
+        )
+        end
+    }
 
     -- Align symbols within vim
     use {
